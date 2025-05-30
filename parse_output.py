01fe4fe622d7e7cf81be0ad0ae20cfd15fdddce8
marcostranisci <marcoantonio.stranisci@unito.it>
2025-05-30 09:10:32 +0200

    added output

--- parse_output.py
+++ parse_output.py
@@ -21,4 +21,16 @@ def parse_output(a,b):
 
 df['parsed_output'] = df.apply(lambda x:parse_output(x.pattern,x.output),axis=1)
 df = df.dropna().drop_duplicates(subset=['id','cleaned_cl_marem','cleaned_cl_marco','cleaned_cl_ale'])
+
+l = list()
+
+for _,item in df.iterrows():
+    if item.parsed_output==item.cleaned_cl_marem:
+        l.append('marem')
+    elif item.parsed_output==item.cleaned_cl_marco:
+        l.append('marco')
+    else:
+        l.append('ale')
+
+df['label'] = l
 df.to_csv('output/parsed_output.csv',index=False)
